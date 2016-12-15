3e11c4763427aff6942e2204119ddba04e34bc86
rachdi <bigking3100@gmail.com>
2016-12-15 16:33:31 +0100

    cv sémantiqué

---
+++ app.js
@@ -0,0 +1,9 @@
+
+
+// $("#arriere").on("click",function(){
+// 	$(".ui.intert.shape").shape("flip left");
+// })
+// $("#avance").on("click",function(){
+// 	$(".ui.intert.shape").shape("flip right");
+// })
+
